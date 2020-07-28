3a4863c19c78d788abfcad0cf5f55f8ee45dc5b6
aguchinwe <chinwebarbara@yahoo.com>
2020-07-28 12:23:18 +0100

    first commit

---
+++ coding-bootcamp-testimonials-slider-master/scripts.js
@@ -0,0 +1,31 @@
+const next  = document.querySelector('.next');
+const prev  = document.querySelector('.prev');
+const slides    = document.querySelectorAll('.slides');
+
+let index = 0;
+display(index);
+function display(index){
+   slides.forEach((slide) =>{
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'flex'; 
+}
+
+function nextSlide () {
+    index++;
+    if(index > slides.length - 1) {
+        index = 0;
+    }
+    display(index);
+}
+function prevSlide () {
+    index--;
+    if(index < 0){ 
+        index= slides.length - 1;
+    }
+    display(index);
+}
+
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
